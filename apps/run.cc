8ca39fd77a8e7e7d410d5397c3ceedec6fb79680
= <brewcrew5498@outlook.com>
2020-06-10 10:42:52 -0500

    Make helper functions.

--- apps/run.cc
+++ apps/run.cc
@@ -10,6 +10,7 @@
 #include <string>
 
 DEFINE_uint32(n, 0, "n");
+DEFINE_bool(by_length, false, "by-length");
 
 using hook_length::HookLengths;
 using hook_length::Partitions;
@@ -23,6 +24,59 @@ std::ostream& operator<<(std::ostream& os, const Partition& partition) {
   return os;
 }
 
+// Prints each partition \lambda along with (f^\lambda)/n! _in order_
+// lexicographically decreasing.
+void PrintPartitions(const Partitions& partitions) {
+  for (const Partition& partition : partitions) {
+    const size_t hl = HookLengths(partition);
+    std::cout << partition << "\t|" << hl << "\n";
+  }
+  std::cout << std::flush;
+}
+
+// Prints each essential partition \lambda along with (f^\lambda)/n! _in order_
+// by decreasing hook lengths.
+void PrintPartitionsByHookLength(const Partitions& partitions) {
+  std::vector<Partition> parts;
+  std::vector<size_t> hls;
+
+  for (const Partition& partition : partitions) {
+    parts.push_back(partition);
+    hls.push_back(HookLengths(partition));
+  }
+
+  // Sort by hook length product.
+  std::vector<size_t> idxs(parts.size());
+  std::iota(idxs.begin(), idxs.end(), 0);
+  std::stable_sort(idxs.begin(), idxs.end(),
+                   [&hls](size_t i, size_t j) { return hls[i] < hls[j]; });
+
+  // Only print dominant partitions (i.e., no conjugates).
+  size_t hlp = 0;
+  std::vector<Partition> saved_parts;
+
+  // Prints each essential partition with hook length denom hlp.
+  auto print = [](std::vector<Partition>& saved_parts, size_t hlp) {
+    const size_t sps = saved_parts.size();
+    if (sps % 2 == 1 && sps > 1) {
+      std::cerr << "something is wrong here" << std::endl;
+    }
+    for (size_t j = 0; j < (sps + 1) / 2; ++j) {
+      std::cout << saved_parts[j] << "\t| " << hlp << std::endl;
+    }
+  };
+
+  for (size_t i : idxs) {
+    if (hls[i] != hlp) {
+      print(saved_parts, hlp);
+      hlp = hls[i];
+      saved_parts.clear();
+    }
+    saved_parts.push_back(parts[i]);
+  }
+  print(saved_parts, hlp);
+}
+
 int main(int argc, char** argv) {
   gflags::SetUsageMessage("Prints each partition of n");
   gflags::ParseCommandLineFlags(&argc, &argv, true);
@@ -33,49 +87,10 @@ int main(int argc, char** argv) {
   }
 
   const Partitions partitions{FLAGS_n};
-  std::vector<Partition> parts;
-  std::vector<size_t> hls;
+  const auto printFn =
+      FLAGS_by_length ? PrintPartitionsByHookLength : PrintPartitions;
 
-  for (const Partition& partition : partitions) {
-    // std::cout << partition << std::endl;
-    parts.push_back(partition);
-    hls.push_back(HookLengths(partition));
-  }
-
-  // Sort by hook length product.
-  std::vector<size_t> idxs(parts.size());
-  std::iota(idxs.begin(), idxs.end(), 0);
-  std::stable_sort(idxs.begin(), idxs.end(),
-      [&hls](size_t i, size_t j) { return hls[i] < hls[j]; }
-  );
-
-  // Only print dominant partitions (i.e., no conjugates).
-  size_t hlp = 0;
-  std::vector<Partition> saved_parts;
-  for (size_t i : idxs) {
-    if (hls[i] != hlp) {
-
-      size_t sps = saved_parts.size();
-      if (sps % 2 == 1 && sps > 1) {
-        std::cerr << "something is wrong here" << std::endl;
-      }
-      for (size_t j = 0; j < (sps+1)/2; ++j) {
-        std::cout << saved_parts[j] << "\t| " << hlp << std::endl;
-      }
-
-      hlp = hls[i];
-      saved_parts.clear();
-    }
-    saved_parts.push_back(parts[i]);
-  }
-
-  size_t sps = saved_parts.size();
-  if (sps % 2 == 1 && sps > 1) {
-    std::cerr << "something is wrong here" << std::endl;
-  }
-  for (size_t j = 0; j < (sps+1)/2; ++j) {
-    std::cout << saved_parts[j] << "\t| " << hlp << std::endl;
-  }
+  printFn(partitions);
 
   return EXIT_SUCCESS;
 }
